0f2d88619764ce29cad49156c554a3926f3f22bf
Max <maxsiepsiak238@gmail.com>
2025-11-25 10:46:31 +0100

    Init h1

---
+++ src/p1/P1_main.java
@@ -0,0 +1,11 @@
+package p1;
+
+public class P1_main {
+    public static void main(String[] args) {
+        System.out.println(square(-2));
+    }
+
+    public static int square(int a) {
+        return ((a+1) * (a+1));
+    }
+}
